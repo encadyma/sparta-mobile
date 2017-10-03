47b45b1f48f30a56d13c8651c7c4c8862990e83b
encadyma <encadyma@gmail.com>
2017-10-03 16:45:38 -0700

    Tab restyling

---
+++ src/pages/list/list.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+
+export class ListPage implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ src/pages/search/search.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-search',
+  templateUrl: 'search.html'
+})
+
+export class SearchPage implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
