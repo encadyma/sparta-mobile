da8cc7f255648c8ad2d2e27fd0b6ec46e84cde54
encadyma <encadyma@gmail.com>
2017-10-08 17:27:27 -0700

    Added OneSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component, Injectable, ViewChild } from '@angular/core';
 import { Platform, NavController, Tabs } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { OneSignal } from '@ionic-native/onesignal';
 
 import { HomePage } from '../pages/home/home';
 import { SearchPage } from '../pages/search/search';
 import { ListPage } from '../pages/list/list';
+
+import { ONESIGNAL_KEY_ID, FIREBASE_KEY_ID } from './secret';
 
 @Component({
   templateUrl: 'app.html'
@@ -17,12 +20,28 @@ export class MyApp {
   searchPage: any = SearchPage;
   listPage: any = ListPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, oneSignal: OneSignal) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      // Initialize OneSignal
+      if (platform.is('cordova')) {
+        oneSignal.startInit(ONESIGNAL_KEY_ID, FIREBASE_KEY_ID);
+        oneSignal.inFocusDisplaying(oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+        // Event listeners
+        oneSignal.handleNotificationReceived().subscribe(() => {
+          // Notification recieved
+        });
+
+        oneSignal.handleNotificationOpened().subscribe(() => {
+          // Notification opened
+        });
+
+        oneSignal.endInit();
+      }
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CategoryListPage } from '../pages/category-list/category-list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SocialSharing } from '@ionic-native/social-sharing';
+import { OneSignal } from '@ionic-native/onesignal';
 
 @NgModule({
   declarations: [
@@ -45,6 +46,7 @@ import { SocialSharing } from '@ionic-native/social-sharing';
     StatusBar,
     SplashScreen,
     SocialSharing,
+    OneSignal,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
